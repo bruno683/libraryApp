fa998903f7cf8e516da3a5639656f25763b07816
Bruno Richard <bruno683@outlook.fr>
2021-10-09 12:34:01 +0200

    Add user and books entity

---
+++ src/Entity/Books.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BooksRepository::class)
+ */
+class Books
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAvailable;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $getAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $getBackAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsAvailable(): ?bool
+    {
+        return $this->isAvailable;
+    }
+
+    public function setIsAvailable(bool $isAvailable): self
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    public function getGetAt(): ?\DateTimeInterface
+    {
+        return $this->getAt;
+    }
+
+    public function setGetAt(?\DateTimeInterface $getAt): self
+    {
+        $this->getAt = $getAt;
+
+        return $this;
+    }
+
+    public function getGetBackAt(): ?\DateTimeInterface
+    {
+        return $this->getBackAt;
+    }
+
+    public function setGetBackAt(?\DateTimeInterface $getBackAt): self
+    {
+        $this->getBackAt = $getBackAt;
+
+        return $this;
+    }
+}
