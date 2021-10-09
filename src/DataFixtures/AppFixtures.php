facac2ce76466ad4c0c329d805636ad0396b338d
Bruno Richard <bruno683@outlook.fr>
2021-10-09 14:17:58 +0200

    Add $employee in fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager )
+    {
+
+        $employee = new User();
+        $employee->setLastName('Richard')
+                ->setFirstName('Bruno')
+                ->setEmail('b.richard@test.com')
+                ->setRoles(['ROLE_EMPLOYEE'])
+                ->setPassword($this->passwordHasher->hashPassword(
+                    $employee,
+                    'Admin72'
+                ));
+        
+        $manager->persist($employee);
 
         $manager->flush();
     }
