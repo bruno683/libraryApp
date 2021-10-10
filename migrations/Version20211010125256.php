54c3b1535d11fe25cb38c190b756bfa0edde59db
Bruno Richard <bruno683@outlook.fr>
2021-10-10 14:55:25 +0200

    Make new migration

---
+++ migrations/Version20211010125256.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211010125256 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cover_img (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE books ADD img_id INT NOT NULL');
+        $this->addSql('ALTER TABLE books ADD CONSTRAINT FK_4A1B2A92C06A9F55 FOREIGN KEY (img_id) REFERENCES cover_img (id)');
+        $this->addSql('CREATE INDEX IDX_4A1B2A92C06A9F55 ON books (img_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE books DROP FOREIGN KEY FK_4A1B2A92C06A9F55');
+        $this->addSql('DROP TABLE cover_img');
+        $this->addSql('DROP INDEX IDX_4A1B2A92C06A9F55 ON books');
+        $this->addSql('ALTER TABLE books DROP img_id');
+    }
+}
