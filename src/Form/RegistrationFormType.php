9587bfce9db2d98233a200fd6bcc96b1a9b64c60
Bruno Richard <bruno683@outlook.fr>
2021-10-11 11:01:40 +0200

    Modified registrationFormType

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -34,14 +34,7 @@ class RegistrationFormType extends AbstractType
                 'format' => 'y-M-d'
             ])
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -59,7 +52,16 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
         ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
