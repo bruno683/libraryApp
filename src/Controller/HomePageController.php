43bf5c938c723622e60a2f328966f166c5ef4d74
Bruno Richard <bruno683@outlook.fr>
2021-10-11 17:20:11 +0200

    Modified homePage.html.twig

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Books;
+use App\Repository\BooksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
-    public function index(): Response
+    public function index(BooksRepository $booksRepository): Response
     {
+
+        $books = $booksRepository->findAll();
+       
+
         return $this->render('home_page/index.html.twig', [
             'title' => 'Catalogue',
+            'books' => $books,
+            
             
         ]);
     }
